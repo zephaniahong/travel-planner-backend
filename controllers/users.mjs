01fce9912a3249a3a5d4c2f500f714d21b906552
Jchu4 <jerome.chuame@gmail.com>
2021-05-25 12:14:48 +0800

    Update userTrips route. Enable AllTrips component to showcase user's trips

--- controllers/users.mjs
+++ controllers/users.mjs
@@ -1,11 +1,14 @@
 export default function initUsersController(db) {
   const userTrips = async (req, res) => {
     try {
-      const trips = await db.User.findOne({
+      // Hard code user id = 1 for MVP.
+      const trips = await db.Trip.findAll({
         where: {
-          id: 1,
+          userId: 1,
         },
       });
+
+      console.log("See user id 1's trips: ---", trips);
 
       res.send(trips);
     } catch (err) {
