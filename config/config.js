cb55b05a258c3074eed15675a66378b74421378a
Zephaniah <zealorez@gmail.com>
2021-05-31 11:41:23 +0800

    reconfigure for heroku deployment

--- config/config.js
+++ config/config.js
@@ -6,18 +6,4 @@ module.exports = {
     host: '127.0.0.1',
     dialect: 'postgres',
   },
-  test: {
-    username: 'zephaniahong',
-    password: null,
-    database: 'travel_planner_test',
-    host: '127.0.0.1',
-    dialect: 'postgres',
-  },
-  production: {
-    username: 'zephaniahong',
-    password: null,
-    database: 'travel_planner_production',
-    host: '127.0.0.1',
-    dialect: 'postgres',
-  },
 };
