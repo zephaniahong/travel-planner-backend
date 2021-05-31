3dc55759be0700a98fcb6395496f8356313f39cb
Jchu4 <jerome.chuame@gmail.com>
2021-05-31 15:42:22 +0800

    Add url library

--- models/index.mjs
+++ models/index.mjs
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
+import url from 'url';
 import allConfig from '../config/config.js';
-
 import initItemModel from './item.mjs';
 import initReviewModel from './review.mjs';
 import initTripModel from './trip.mjs';
