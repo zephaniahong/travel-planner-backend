59589dae36c43f18054b0ffaa997d62368de2702
Zephaniah <zealorez@gmail.com>
2021-05-31 11:30:24 +0800

    change database name

--- config/config.js
+++ config/config.js
@@ -9,14 +9,14 @@ module.exports = {
   test: {
     username: 'zephaniahong',
     password: null,
-    database: 'travel_planner_development',
+    database: 'travel_planner_test',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
   production: {
     username: 'zephaniahong',
     password: null,
-    database: 'travel_planner_development',
+    database: 'travel_planner_production',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
